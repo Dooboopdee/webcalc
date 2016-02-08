0c8cb98d867dc2af2314a4a818ca5251b10e01ee
Dooboopdee <johntgarrison@gmail.com>
2016-02-08 12:31:16 -0600

    Created random quote functionality, changed name to g-slang machine ;)

--- scripts/randquote.js
+++ scripts/randquote.js
@@ -26,8 +26,45 @@ $('#getQuoteBtn').click(function() {
    			return color;
    		}
     }
-    // Get random quote from API
-    var quote = 'This is still under construction yo ;)';
+
+    function randomQuote() {
+      var beginningWords = [
+      'The', 'a'
+      ];
+      var beginningWord = beginningWords[Math.floor(Math.random() * beginningWords.length)];
+
+      var connectingWords = [
+      'was', 'could', 'so', 'nigga be', 'then'
+      ];
+      var connectedWord = connectingWords[Math.floor(Math.random() * connectingWords.length)];
+
+      var nouns = [
+      'house','car','bowl','cup','paper','mouse','camel','coffee pot','leaf','goldfish','straw',
+      'laptop', 'life journey', 'adventure', 'cupholder', 'tree', 'plant', 'elephant', 'sheep',
+      'mushroom', 'carrot', 'potato', 'aqaurium', 'shark', 'fish', 'tuna', 'table', 'chair'
+      ];
+      var noun = nouns[Math.floor(Math.random() * nouns.length)];
+
+      var begAdjectives = [
+      'mystical', 'radical', 'suplendous', 'splendid', 'terrific', 'groovy'
+      ];
+      var begAdjective = begAdjectives[Math.floor(Math.random() * begAdjectives.length)];
+
+      var verbs = [
+      'hardcore sprinted', 'slippin and ate shit. ', 'legendary galloped like a terrific G.', 'hopped and popped,',
+      'ride real eazy like. ', 'lacking smacking, ripping and dippin.'
+      ];
+      var verb = verbs[Math.floor(Math.random() * verbs.length)];
+
+      var endAdjectives = [
+      'radical crazy stuff. yeah.', '- crazy', '- sick nasty.',' rad.', 'magically', 'mystically', 'crazy yo.'
+      ];
+      var endAdjective = endAdjectives[Math.floor(Math.random() * endAdjectives.length)];
+
+      var sentence = ' ' + beginningWord + ' ' + begAdjective + ' ' + noun + ' ' + connectedWord + ' ' + verb + ' ' + endAdjective + '.  ';
+
+      return sentence;
+    }
 
     // JQuery to reassign the values from above to the correct css properties
     $(this).css('background-color',getRandomColor());
@@ -35,5 +72,5 @@ $('#getQuoteBtn').click(function() {
     $('#getQuoteBtn:hover').css('box-shadow: 0px 0px ', getRandomColor());
     $('#getQuoteBtn:active').css('box-shadow: 0px 0px ', getRandomColor());
     // Put quote in screen display div for output
-    $('.screenQuote').append(quote);
+    $('.screenQuote').append(randomQuote());
 });
