6c001bbbc4ac15de39f8ec8d4e4b5150e2a9471e
Dooboopdee <johntgarrison@gmail.com>
2016-02-08 09:17:53 -0600

    Added random quote script and html/css stuff

---
+++ scripts/randquote.js
@@ -0,0 +1,36 @@
+// RANDOM QUOTE c JOHN GARRISON
+$('#getQuoteBtn').click(function() {
+	// Get random color for background-color of button
+	function getRandomColor() {
+    	var letters = '0123456789ABCDEF'.split('');
+    	var color = '#';
+    	for (var i = 0; i < 6; i++ ) {
+        	color += letters[Math.floor(Math.random() * 16)];
+   		}
+
+    	return color;
+    }
+    // Get random color that doesn't match bg color of btn
+    function getQuoteTxtColor() {
+    	var backGColor = getRandomColor();
+
+    	var letters = '0123456789ABCDEF'.split('');
+    	var color = '#';
+    	for (var i = 0; i < 6; i++ ) {
+        	color += letters[Math.floor(Math.random() * 16)];
+   		}
+
+   		if (backGColor == color) {
+   			getQuoteTxtColor();
+   		} else {
+   			return color;
+   		}
+    }
+
+
+    // JQuery to reassign the values from above to the correct css properties
+    $(this).css('background-color',getRandomColor());
+    $('#getQuoteTxt').css('color', getQuoteTxtColor());
+    $('#getQuoteBtn:hover').css('box-shadow: 0px 0px ', getRandomColor());
+    $('#getQuoteBtn:active').css('box-shadow: 0px 0px ', getRandomColor());
+});
