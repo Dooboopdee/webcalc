6bd0fd016b9db5aa551919a6eaae495dc9382991
Dooboopdee <johntgarrison@gmail.com>
2016-02-08 06:36:57 -0600

    Redone w/ fixes for multiple apps

--- calc.js
+++ calc.js
@@ -9,7 +9,7 @@ var decimalAdded = false;
 for(var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(e) {
 		// Get the input and button values
-		var input = document.querySelector('.screen');
+		var input = document.querySelector('.screenCalc');
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
 		
